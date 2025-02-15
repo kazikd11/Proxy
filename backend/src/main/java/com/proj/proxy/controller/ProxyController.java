b484a7a698754741ce286f0140b3c71b2750e165
Michał Kaźmierczak <kmiki246@gmail.com>
2025-02-15 17:03:17 +0100

    em?

--- backend/src/main/java/com/proj/proxy/controller/ProxyController.java
+++ backend/src/main/java/com/proj/proxy/controller/ProxyController.java
@@ -1,17 +1,17 @@
 package com.proj.proxy.controller;
 
 import com.proj.proxy.service.ProxyService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProxyController {
 
-    @Autowired
-    private ProxyService proxyService;
+    private final ProxyService proxyService;
+
+    public ProxyController(ProxyService proxyService) {
+        this.proxyService = proxyService;
+    }
 
     @GetMapping("/proxy")
     public ResponseEntity<String> proxyRequest(@RequestParam String url) {
@@ -19,5 +19,13 @@ public class ProxyController {
             return ResponseEntity.badRequest().body("URL is required");
         }
         return proxyService.proxy(url);
+    }
+
+    @PostMapping("/proxy")
+    public ResponseEntity<String> proxyPostRequest(@RequestParam String url, @RequestBody String body) {
+        if (url == null || url.isEmpty()) {
+            return ResponseEntity.badRequest().body("URL is required");
+        }
+        return proxyService.proxy(url, body);
     }
 }
--- backend/src/main/java/com/proj/proxy/service/ProxyService.java
+++ backend/src/main/java/com/proj/proxy/service/ProxyService.java
@@ -1,8 +1,10 @@
 package com.proj.proxy.service;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 
 @Service
 public class ProxyService {
@@ -15,6 +17,37 @@ public class ProxyService {
 
     public ResponseEntity<String> proxy(String url) {
         ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        System.out.println("Response from " + url + ": " + response.getBody());
+        if(response.getBody()==null){
+            return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+        }
+        return ResponseEntity.ok(modifyLinks(response.getBody(), getBaseUrl(url)));
+    }
+
+    public ResponseEntity<String> proxy(String url, String body) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>(body, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
         return ResponseEntity.ok(response.getBody());
+    }
+
+    private String modifyLinks(String html, String baseUrl) {
+        String modifiedHtml = html.replaceAll("href=\"/", "href=\"" +"/proxy?url=" + baseUrl + "/");
+        modifiedHtml = modifiedHtml.replaceAll("src=\"/", "src=\""+"/proxy?url=" + baseUrl + "/");
+        modifiedHtml = modifiedHtml.replaceAll("action=\"/", "action=\""+"/proxy?url=" + baseUrl + "/");
+
+        return modifiedHtml;
+    }
+
+
+    public String getBaseUrl(String url) {
+        try {
+            URI uri = new URI(url);
+            return uri.getScheme() + "://" + uri.getHost();
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
